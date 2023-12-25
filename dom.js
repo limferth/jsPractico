cf8e0bcc87ef7731371573c1892907770e31e0dc
limferth <limferth@gmail.com>
2023-12-25 12:38:14 -0600

    Eventos en javascript, interactuando con el usuario

--- dom.js
+++ dom.js
@@ -3,36 +3,13 @@
 */ 
 
 const h1 = document.querySelector("h1");
-const p = document.querySelectorAll("p");
-const parrafito = document.querySelector(".parrafito");
-const pid = document.querySelector('#pid');
-const input = document.querySelector("input");
+const input1 = document.querySelector("#calculo1");
+const input2 = document.querySelector('#calculo2');
+const botonCalcular = document.querySelector('#botonCalcular');
+const pResult = document.querySelector('#result')
 
-console.log(input.value="123");
-
-console.log({
-    h1,
-    p,
-    parrafito,
-    pid,
-    input,
-});
-
-h1.innerHTML = 'limferth es alfa <br> feo'
-// console.log(h1.getAttribute('class')); // sirve apara llamar atributos
-// console.log(h1.setAttribute('class', 'mine')) // sirve para modificar atributos
-
-h1.classList.add('rojo') // para agregar atributos
-h1.classList.remove('rojo')
-// h1.classList.toggle('rojo')
-//h1.classList.contains('rojo')
-
-
-// console.log(document.createElement('img'))
-
-const img = document.createElement('img');
-
-img.setAttribute('src', 'https://www.solvetic.com/uploads/monthly_05_2019/tutorials-9832-0-55361300-1557215199.png')
-
-console.log(img);
-pid.append(img);
+function botonOnClick (){
+    // console.log('escuchando el evento de click')
+    const sumaInputs = +input1.value + +input2.value;
+    pResult.innerText = 'Resultado: ' + sumaInputs;
+}
