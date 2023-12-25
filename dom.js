356c1858bca6d63a8e45b91f38990ffe4d99275b
limferth <limferth@gmail.com>
2023-12-25 13:10:18 -0600

    curso de addEventListener

--- dom.js
+++ dom.js
@@ -6,9 +6,14 @@ const h1 = document.querySelector("h1");
 const input1 = document.querySelector("#calculo1");
 const input2 = document.querySelector('#calculo2');
 const botonCalcular = document.querySelector('#botonCalcular');
-const pResult = document.querySelector('#result')
+const pResult = document.querySelector('#result');
+const form = document.querySelector('#formulario')
 
-function botonOnClick (){
+form.addEventListener('submit', sumaInputValues);
+
+function sumaInputValues (event){
+    console.log({event})
+    event.preventDefault();
     // console.log('escuchando el evento de click')
     const sumaInputs = +input1.value + +input2.value;
     pResult.innerText = 'Resultado: ' + sumaInputs;
