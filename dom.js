24f9c8ae30693a61a3a4a9f429c22a9a83f2f2c5
limferth <limferth@gmail.com>
2023-12-24 19:39:36 -0600

    leyendo y escribiendo el DOM

--- dom.js
+++ dom.js
@@ -1,11 +1,38 @@
-// const h1 = document.querySelector("h1");
-// const p = document.querySelector("p");
-// const parrafito = document.querySelector(".parrafito");
-// const pid = document.querySelector('#pid');
-// const input = document.querySelector("input");
-// // console.log(h1);
-// /*
-// btn .add
+/*
 
-// */
-// pid.addEventListener('click', btnOnClick);
+*/ 
+
+const h1 = document.querySelector("h1");
+const p = document.querySelectorAll("p");
+const parrafito = document.querySelector(".parrafito");
+const pid = document.querySelector('#pid');
+const input = document.querySelector("input");
+
+console.log(input.value="123");
+
+console.log({
+    h1,
+    p,
+    parrafito,
+    pid,
+    input,
+});
+
+h1.innerHTML = 'limferth es alfa <br> feo'
+// console.log(h1.getAttribute('class')); // sirve apara llamar atributos
+// console.log(h1.setAttribute('class', 'mine')) // sirve para modificar atributos
+
+h1.classList.add('rojo') // para agregar atributos
+h1.classList.remove('rojo')
+// h1.classList.toggle('rojo')
+//h1.classList.contains('rojo')
+
+
+// console.log(document.createElement('img'))
+
+const img = document.createElement('img');
+
+img.setAttribute('src', 'https://www.solvetic.com/uploads/monthly_05_2019/tutorials-9832-0-55361300-1557215199.png')
+
+console.log(img);
+pid.append(img);
---
+++ functions.js
@@ -0,0 +1,21 @@
+// Declarativas 
+
+function miFuncion (){
+    return 3;
+}
+
+miFuncion();
+// Expresion 
+
+let miFunction = function (a,b){
+    return a + b;
+}
+
+miFunction();
+
+
+// ejemplo
+function sumar(a,b){
+    var resultado = a + b;
+    return resultado;
+}
